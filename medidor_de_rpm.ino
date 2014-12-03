d44106ab78a999537b51580810f4d4c8a8693e4b
David Sanchez <davids4nchez@gmail.com>
2014-12-03 09:31:27 -0600

    Codigo Arduino Medidor RPM

--- medidor_de_rpm.ino
+++ medidor_de_rpm.ino
@@ -1,11 +1,12 @@
 const int hardwareCounterPin = 5;   // Pin de lectura de señal mediante Timer1 
-const int PinOut             = 13;  // Pin de salida para señal que muestre 
+int PinOut = 11;  // Pin de salida para señal que muestre 
                                     //  tiempo de lectura del Arduino UNO
 
 const int Period = 1000;   // A Period se le asigna un valor de 1000 (milisegundos)
 unsigned int count;
 unsigned int rpm;
-
+int Val=0;
+int Valr=0;
 void setup()
 {
   Serial.begin(9600);      // Se define seria para su uso
@@ -18,9 +19,11 @@ void loop()
 {
   // Segenera la replica de como se hace conteo donde el alto representa 
   //  el conteo de pulsos
-  digitalWrite(PinOut, LOW);
-  delay(Period);
-  digitalWrite(PinOut, HIGH);  
+ 
+   
+  
+ 
+  
   // Inicio de conteo
   bitSet(TCCR1B ,CS12);    // Counter Clock source is external pin
   bitSet(TCCR1B ,CS11);    // Clock on rising edge  
@@ -30,8 +33,12 @@ void loop()
   count = TCNT1;           // Asigna el valor de Timer1 que seria el numero 
                            // de pulsos que lee el Timer1 en 1000 milisegundos 
   TCNT1 = 0;               // Reinicia el contador 
-  rpm=(count*60)/12;       // Operacion que combierte la cantidad de pulsos leida 
-                           //  a rpm
-     Serial.print(rpm);    // Muestra dato de RPM en Serial
-     Serial.println(" RPM");
+  rpm=(count)/12;      // Operacion que combierte la cantidad de pulsos leida 
+     
+     
+                         //  a rpm
+  Serial.println(rpm);    // Muestra dato de RPM en Serial
+  Val= Serial.parseInt();
+  Valr=Val*7.5;
+  analogWrite(PinOut,Valr);
 }
